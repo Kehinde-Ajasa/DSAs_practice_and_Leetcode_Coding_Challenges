ee2bb76e927098f0a455a2cfdf9953cc3d60fa75
Kehinde Ajasa <ti664469@gmail.com>
2023-04-28 18:57:58 +0100

    Exception to catch input of any other type aside from integers

--- Challenge_1(Two Sum).py
+++ Challenge_1(Two Sum).py
@@ -4,14 +4,18 @@ adds up to a particular target."""
 create a dynamic empty dictionary and allow individual items be the key and the index as the value
 if V appears in the dictionary, return, loop value for V and the value of the key of V that appeared"""
 
+try:
 
-def two_sum(array_of_numbers: list[int], target: int) -> list:
-    """Function to help solve the challenge."""
-    dictionary = {}
-    for i in range(len(array_of_numbers)):
-        difference = target - array_of_numbers[i]
-        if difference in dictionary:
-            return [dictionary[difference], i]
-        dictionary[array_of_numbers[i]] = i
 
-print(two_sum([4,7,8,9], 12))
+    def two_sum(array_of_numbers: list[int], target: int) -> list:
+        """Function to help solve the challenge."""
+        dictionary = {}
+        for i in range(len(array_of_numbers)):
+            difference = target - array_of_numbers[i]
+            if difference in dictionary:
+                return [dictionary[difference], i]
+            dictionary[array_of_numbers[i]] = i
+
+
+except TypeError:
+    print("Dear User, Ensure you only have integers in the array and as the target")
