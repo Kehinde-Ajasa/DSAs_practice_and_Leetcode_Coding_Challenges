56dacc62e2cc36ddcdb55ee197cac11e17260b39
Kehinde Ajasa <ti664469@gmail.com>
2023-04-28 22:01:08 +0100

    Revised code from Leetcode and updated source code in an OOP manner

--- Challenge_258(Add Digits).py
+++ Challenge_258(Add Digits).py
@@ -1,33 +1,11 @@
-"""This program solves a challenge that keeps adding a nth digit number until its result
-is another nth digit number long"""
+class Solution:
 
-try:
-	output = ""
-
-	def get_input():
-		"""Function to accept all user's input"""
-		global output
-		main_number = int(input("Please enter the number you would like to add: "))
-		output = int(input("How many digits do you want your result to be: "))
-		return main_number
-
-
-	try:
-		def add_digits_to_target(user_value):
-			summation = 0
-			for numbers in str(user_value):
-				summation += int(numbers)
-			if len(str(summation)) != output:
-				return add_digits_to_target(summation)
-			else:
-				return summation
-
-
-		print(add_digits_to_target(get_input()))
-	except RecursionError:
-		print("Sorry, error while preparing result,\nEnsure that the length of the result \
-is less than or equal to the length of the input")
-
-except ValueError:
-	print("Dear user, we encountered minor issue while preparing your result\
-please check your input and ensure they are all integers")
+    def add_digits(self, num: int) -> int:
+        string_val = str(num)
+        print(string_val)
+        addition = 0
+        for i in string_val:
+            addition += int(i)
+        if len(str(addition)) != 1:
+            return self.add_digits(addition)
+        return addition
