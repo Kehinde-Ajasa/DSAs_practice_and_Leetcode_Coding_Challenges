649c44b0da0cbd22b2462c4d60af827e5f293ed6
Kehinde Ajasa <ti664469@gmail.com>
2023-04-30 01:17:17 +0100

    Unit Testing for challenge 258

---
+++ test_challenge_258.py
@@ -0,0 +1,7 @@
+from challenge_258 import add_digits
+
+
+def test_add_digits():
+    """testing the return value of the function add_digit()"""
+    for i in range(1, 100):
+        assert len(str(add_digits(i))) == 1
