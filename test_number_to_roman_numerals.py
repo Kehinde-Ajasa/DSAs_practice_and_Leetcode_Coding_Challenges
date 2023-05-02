53c64d435032a5e8c1cc43b957c93e9e18c6471c
Kehinde Ajasa <ti664469@gmail.com>
2023-05-03 00:39:29 +0100

    Created test to check if the standard roman items are correct

--- test_number_to_roman_numerals.py
+++ test_number_to_roman_numerals.py
@@ -1,3 +1,11 @@
 from number_to_roman_numerals import standards
 
 
+def test_roman_standards():
+    assert standards(1) == "I"
+    assert standards(5) == "V"
+    assert standards(10) == "X"
+    assert standards(50) == "L"
+    assert standards(100) == "C"
+    assert standards(500) == "D"
+    assert standards(1000) == "M"
